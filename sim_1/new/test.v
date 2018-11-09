55355824d262b6da338623e1acdf34690b9d1387
Heron Zhang <byheronzhang@163.com>
2018-11-09 11:53:51 +0800

    regfile完成！

--- sim_1/new/test.v
+++ sim_1/new/test.v
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> parent of b5c4b63... ..
 module test(
     );
-reg [4:0]Wr,Ra,Rb;
-reg [31:0]D;
-reg We,Clk,Clrn;
-wire [31:0]Qa,Qb;
+    reg [4:0]Ra,Rb,Wr;
+    reg [31:0]D;
+    reg We,Clk,Clrn;
+    wire [31:0]Qa,Qb;
 
 REGFILE uut(.Ra(Ra),
 			.Rb(Rb),
@@ -23,8 +17,16 @@ REGFILE uut(.Ra(Ra),
 			);
 initial 
 	begin
-		Wr=5'd0;
+	
+		We=1;
+		Clrn=1;
 		D=32'd666;
+		Ra=5'd3;
+		Wr=5'd3;
+		#100
+		D=32'h666;
+		Rb=5'd4;
+		Wr=5'd4;
 
 	end
 always
@@ -32,84 +34,7 @@ begin
 
 		#20 Clk = 0;
 
-<<<<<<< HEAD
-=======
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2018/11/07 23:17:56
-// Design Name: 
-// Module Name: test
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
->>>>>>> parent of a621d5b... 整理
-module test(
-    );
-reg [31:0]X,Y;
-reg [1:0]Aluc;
-wire [31:0]R;
-wire Z;
-
-<<<<<<< HEAD
-=======
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2018/11/07 23:17:56
-// Design Name: 
-// Module Name: test
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
-module test(
-    );
-reg [31:0]X,Y;
-reg [1:0]Aluc;
-wire [31:0]R;
-wire Z;
-
->>>>>>> parent of a621d5b... 整理
-=======
->>>>>>> parent of a621d5b... 整理
-ALU uut(.X(X),
-		.Y(Y),
-		.Aluc(Aluc),
-		.R(R),
-		.Z(Z));
-initial begin
-	X=32'b1100;
-	Y=32'b1010;
-	Aluc=2'b10;
-=======
 		#20 Clk = 1;
->>>>>>> parent of b5c4b63... ..
 
 end
 endmodule
--- sources_1/alu/MUX2X1.v
+++ sources_1/alu/MUX2X1.v
@@ -4,5 +4,5 @@ module MUX2X1(A0,A1,S,Y);
 	not i0(S_n,S);
 	nand i1(A0_S,A0,S_n);
 	nand i2(A1_S,A1,S);
-	nor i3(Y,A0_S,A1_S);
+	nand i3(Y,A0_S,A1_S);
 endmodule
